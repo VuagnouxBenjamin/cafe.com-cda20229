2780b9ee48acf4394a285ae0267e2666406ad655
Benjamin Vuagnoux <vuagnoux.benjamin1@gmail.com>
2021-09-13 14:15:19 +0200

    feat(product-list) : controller

---
+++ src/Controller/ProductListController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductListController extends AbstractController
+{
+    /**
+     * @Route("/products", name="product_list")
+     */
+    public function index(): Response
+    {
+        return $this->render('product_list/index.html.twig', [
+            'controller_name' => 'ProductListController',
+        ]);
+    }
+}
