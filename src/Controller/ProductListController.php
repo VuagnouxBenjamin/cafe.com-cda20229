829f90620ce4ace1c4ee350be334af8fb64190b8
Benjamin Vuagnoux <vuagnoux.benjamin1@gmail.com>
2021-09-13 14:26:48 +0200

    feat(product-list): getting data via controller

--- src/Controller/ProductListController.php
+++ src/Controller/ProductListController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Products;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,12 +13,14 @@ class ProductListController extends AbstractController
     /**
      * @Route("/categorie-{id}", name="product_list")
      */
-    public function index($id): Response
+    public function index($id, EntityManagerInterface $entityManager): Response
     {
+
 
 
         return $this->render('product_list/index.html.twig', [
             'controller_name' => 'ProductListController',
+            'products' => $entityManager->getRepository(Products::class)->findBy(['categorie' => $id]),
         ]);
     }
 }
