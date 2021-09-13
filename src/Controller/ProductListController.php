aaf985b7399d23b160328602633a25a7134ee8fa
Benjamin Vuagnoux <vuagnoux.benjamin1@gmail.com>
2021-09-13 14:44:46 +0200

    feat(product-list): pagination back-end

--- src/Controller/ProductListController.php
+++ src/Controller/ProductListController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Products;
 use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,14 +15,21 @@ class ProductListController extends AbstractController
     /**
      * @Route("/categorie-{id}", name="product_list")
      */
-    public function index($id, EntityManagerInterface $entityManager): Response
+    public function index($id, EntityManagerInterface $entityManager, PaginatorInterface $paginator, Request $request): Response
     {
+        $products = $entityManager->getRepository(Products::class)->findBy(['categorie' => $id]);
 
+        $pagination = $paginator->paginate(
+            $entityManager->getRepository(Products::class)->findByCategoryQuery($id),
+            $request->query->getInt('page', 1),
+            12
+        );
 
 
         return $this->render('product_list/index.html.twig', [
             'controller_name' => 'ProductListController',
-            'products' => $entityManager->getRepository(Products::class)->findBy(['categorie' => $id]),
+            'products' => $products,
+            'pagination' => $pagination,
         ]);
     }
 }
