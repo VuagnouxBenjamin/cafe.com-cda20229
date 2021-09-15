f10a90b72e78fb46665c07ca63b36d57e4474932
Benjamin Vuagnoux <vuagnoux.benjamin1@gmail.com>
2021-09-15 19:19:31 +0200

    fix(pull) : missing migration

---
+++ migrations/Version20210915171233.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210915171233 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_DBFED89DE7927C74 ON email_list (email)');
+        $this->addSql('ALTER TABLE products ADD image VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX UNIQ_DBFED89DE7927C74 ON email_list');
+        $this->addSql('ALTER TABLE products DROP image');
+    }
+}
